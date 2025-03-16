2cfa3abcf80e8678d3030a7858aed28a6e8f62ec
Shannon Lal <shannonlal@hotmail.com>
2025-03-16 16:24:35 -0400

    Fix formating issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,17 +33,17 @@ module.exports = {
         'fade-in-up': {
           '0%': {
             opacity: '0',
-            transform: 'translateY(20px)'
+            transform: 'translateY(20px)',
           },
           '100%': {
             opacity: '1',
-            transform: 'translateY(0)'
-          }
-        }
+            transform: 'translateY(0)',
+          },
+        },
       },
       animation: {
-        'fade-in-up': 'fade-in-up 0.5s ease-out'
-      }
+        'fade-in-up': 'fade-in-up 0.5s ease-out',
+      },
     },
   },
   plugins: [],
